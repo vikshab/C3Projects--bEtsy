d50ebfeb630e0a3324b3cc232a81ee1562931214
Corinne Pingul <corinnepingul@gmail.com>
2015-07-15 14:00:40 -0700

    Deleted the comments at the top of the seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
 sellers = [
   ["user1",  "email1@email.com",  "password1"],
   ["user2",  "email2@email.com",  "password2"],
