e65de9655905270e30ec9e472f249d124c4e00c7
Ashley Watkins <AWatkins31@gmail.com>
2015-07-15 13:54:59 -0700

    Adds Category seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+  categories = [
+    { name: "Grocery" },
+    { name: "Animals" },
+    { name: "Home and Garden" },
+    { name: "Toys" },
+    { name: "Clothing" },
+    { name: "Health and Beauty" },
+    { name: "Sports and Outdoors" },
+    { name: "Automotive" },
+    { name: "Books" },
+    { name: "Movies" },
+    { name: "Electronic" }
+  ]
+
+  categories.each do |category|
+    Category.create(category)
+  end
