70e707bff5eba8ab4ef2d952bc386aff059f704f
Jeri <drvonnjerryxlii@gmail.com>
2015-07-15 14:29:45 -0700

    created some orders content but not sure it's the right way to go (routes & controller actions)

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,26 @@
 class OrdersController < ApplicationController
+  def add
+    # code to add item to the cart
+    # some kind of handling for users trying to circumvent assigned id
+      # eg, checking for pending status & not allowing any changes otherwise
+
+    if true #success
+      # increment cart view
+      # redirect_to product page
+    else # not success
+      # why would it not be successful?
+    end
+  end
+
+  def cart
+    # code to view items in cart
+  end
+
+  def checkout
+    # code to add buyer info
+  end
+
+  def receipt
+    # code to display finalized order
+  end
 end
