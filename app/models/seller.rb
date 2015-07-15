9852fed74809731aaec6738f90e368f5577312a0
Corinne Pingul <corinnepingul@gmail.com>
2015-07-15 15:22:29 -0700

    Added test for and functionality for seller model username validation.

--- app/models/seller.rb
+++ app/models/seller.rb
@@ -1,4 +1,8 @@
 class Seller < ActiveRecord::Base
+  # Validations ----------------------
+  validates :username, presence: true, uniqueness: true 
+
+  # Associations ----------------------
   has_secure_password
   has_many :products
   has_many :order_items, through: :products
--- spec/models/seller_spec.rb
+++ spec/models/seller_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Seller, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "model validations" do
+    let(:seller_w_o_username) { Seller.new(email: "email") }
+    let(:seller_w_o_email) { Seller.new(username: "name") }
+    let(:seller_w_o_password) { Seller.new(username: "name", email: "email") }
+
+
+    it "doesn't save a seller without a name" do
+      seller_w_o_username.password = "hi"
+      seller_w_o_username.password_confirmation = "hi"
+
+      expect(seller_w_o_username).to_not be_valid
+      expect(seller_w_o_username.errors.keys).to include(:username)
+    end
+  end
 end
