b4ba9b468d1094172abc4cb3ebbe80dafad65f74
Brittany L. Walentin <brittany.walentin@gmail.com>
2015-07-15 10:44:34 -0700

    WIP: sessions controller rspec tests for controller actions. No actualy tests created yet, but working on it.

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,4 +2,9 @@ require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
 
+  describe "POST #create" do
+    it "creates a session tied to user_id" do
+    end
+  end
+
 end
