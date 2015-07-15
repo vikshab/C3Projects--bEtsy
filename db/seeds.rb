95ada165c86817e38922274476bb2f79ba730de4
Jeri <drvonnjerryxlii@gmail.com>
2015-07-15 13:59:20 -0700

    added order seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+order_statuses = ["pending", "paid", "completed", "cancelled"]
+buyer_names = [
+  "Johnathan NoLastName", "Satan McHandsomeDevil", "Dr. Potato Head",
+  "Ada Lovelace", "Betty McAwesomePants", "Tallis GenericLastName", "Tux"
+]
+
+50.times do
+  current_status = order_statuses.sample
+
+  if current_status == "pending"
+    Order.create(status: current_status)
+  else
+    current_name = buyer_names.sample
+
+    Order.create(
+      status: current_status,
+      buyer_name: buyer_names.sample,
+      buyer_email: current_name.split(" ").sample + "@email.net",
+      buyer_address: "1234 fake st",
+      buyer_card_short: "4567",
+      buyer_card_expiration: Date.parse("June 5 2016")
+    )
+  end
+end
