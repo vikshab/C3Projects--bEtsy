8c008d78aa8f807b3846127f27dd2ea1abe5f939
Anita <asovay@gmail.com>
2015-07-15 14:03:09 -0700

    fixed model migrations

--- db/migrate/20150714232434_create_order_items.rb
+++ db/migrate/20150714232434_create_order_items.rb
@@ -2,9 +2,9 @@ class CreateOrderItems < ActiveRecord::Migration
   def change
     create_table :order_items do |t|
       t.integer :quantity
-      t.references :order_id, index: true, foreign_key: true
-      t.references :product_id, index: true, foreign_key: true
-      t.references :user_id, index: true, foreign_key: true
+      t.references :order, index: true, foreign_key: true
+      t.references :product, index: true, foreign_key: true
+      t.references :user, index: true, foreign_key: true
 
       t.timestamps null: false
     end
--- db/migrate/20150714232512_create_reviews.rb
+++ db/migrate/20150714232512_create_reviews.rb
@@ -3,7 +3,7 @@ class CreateReviews < ActiveRecord::Migration
     create_table :reviews do |t|
       t.text :body
       t.integer :rating
-      t.references :product_id, index: true, foreign_key: true
+      t.references :product, index: true, foreign_key: true
 
       t.timestamps null: false
     end
