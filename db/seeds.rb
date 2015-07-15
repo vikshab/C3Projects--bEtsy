d42110761ee4248ae9958774bdf3a9bb1eede943
Anita <asovay@gmail.com>
2015-07-15 14:41:06 -0700

    removed user seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,4 @@
 require 'csv'
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
 
 CSV.foreach("db/categories.csv", headers: true) do |row|
 
@@ -13,14 +6,6 @@ CSV.foreach("db/categories.csv", headers: true) do |row|
     name: row[0]
   )
  end
-
- CSV.foreach("db/users.csv", headers: true) do |row|
-
-   User.create(
-     name: row[0],
-     email: row[1]
-   )
-  end
 
 CSV.foreach("db/products.csv", headers: true) do |row|
 
