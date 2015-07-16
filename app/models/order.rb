0801a3735cbad396fe00f60de706b0bed332fd09
Brittany L. Walentin <brittany.walentin@gmail.com>
2015-07-15 20:14:07 -0700

    order model validations.

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,16 @@
 class Order < ActiveRecord::Base
   has_many :order_items
+
+  # VALIDATIONS ------------------------------------------------------------
+  validates :buyer_name, presence: true
+  validates :buyer_email, presence: true
+  validates :buyer_address, presence: true
+  validates :buyer_zip, presence: true
+  validates :buyer_state, presence: true
+  validates :buyer_city, presence: true
+  validates :buyer_expcc, presence: true
+  validates :buyer_last4cc, presence: true
+
+  # SCOPES ------------------------------------------------------------
+
 end
