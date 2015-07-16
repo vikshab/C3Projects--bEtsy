646aca3825d11122374d62b73519b3fff0b39de4
Elia <elgrenier@gmail.com>
2015-07-15 21:53:48 -0700

    working new user sign up form and a couple tests for the controller. error messages works, the success flash is not working right now.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
 
+  def index
+    @users = User.all
+  end
+
   def show
     @user = User.find(params[:id])
   end
@@ -9,8 +13,10 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])
+    @user = User.new(create_params)
     if @user.save
+      flash.now[:success] = "Welcome to Bitsy, #{@user.name}!"
+      redirect_to user_path(@user)
     else
       render 'new'
     end
@@ -18,9 +24,9 @@ class UsersController < ApplicationController
 
   private
 
-  def create_params
-    params.permit(user: [:name, :email, :password_digest])
-  end
+    def create_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 
 
 end
