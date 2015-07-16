89a2e967f66c8bbffdac8e8805bdbc5fbe703c17
Corinne Pingul <corinnepingul@gmail.com>
2015-07-16 15:01:39 -0700

    Wrote spec for instance variable in sellers#index.

--- spec/controllers/sellers_controller_spec.rb
+++ spec/controllers/sellers_controller_spec.rb
@@ -2,27 +2,30 @@ require 'rails_helper'
 
 RSpec.describe SellersController, type: :controller do
   describe "GET #index" do
+    before :each do
+      seller = Seller.new(username: "Harry", email: "harry.potter@email.com")
+      seller.password = "test"
+      seller.password_confirmation = "test"
+      seller.save
+      @seller_id = seller.id
+
+      10.times do
+        Product.create(name: "new", price: 10, seller_id: seller.id, stock: 1)
+      end
+    end
+
     it "responds successfully with an HTTP 200 status code" do
       get :index
 
       expect(response).to be_success
       expect(response).to have_http_status(200)
     end
-  end
 
-  describe "calculate_num_products(seller)" do
-    before :all do
-      @seller = Seller.new(username: "Harry", email: "harry.potter@email.com")
-      @seller.password = "test"
-      @seller.password_confirmation = "test"
-      @seller.save
-      10.times do
-        Product.create(name: "new", price: 10, seller_id: @seller.id)
-      end
-    end
+    it "knows the number of products a seller has in stock" do
+      get :index
+      expected_result = { @seller_id => 10 }
 
-    it "returns the total number of products for a specific seller" do
-      expect(calculate_num_products(@seller)).to eq 7
+      expect(assigns(:num_products_for_sellers)).to eq expected_result
     end
   end
 end
