75e9c6779a0aaec95b54e1e3d7e8d5605cdc27e7
Lindsey Reno <lindseyannereno@gmail.com>
2015-07-16 16:20:51 -0700

    Adds new and create actions

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,23 @@
 class ReviewsController < ApplicationController
+
+  def new
+    @review = Review.new
+    @product = Product.find(params[:id])
+  end
+
+  def create
+    @product = Product.find(params[:id])
+    @review = Review.new(create_params)
+      if @review.save
+        redirect_to product_path(@product)
+      else
+        render :new
+      end
+  end
+
+  private
+
+  def create_params
+    params[:review].permit(:rating, :description, :product_id)
+  end
 end
