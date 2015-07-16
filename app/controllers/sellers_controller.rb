05e49c2f6c9f242f68e494162da3f9079ec69f65
Corinne Pingul <corinnepingul@gmail.com>
2015-07-16 12:45:31 -0700

    Abstracted logic from view file into controller.

--- app/controllers/sellers_controller.rb
+++ app/controllers/sellers_controller.rb
@@ -1,9 +1,31 @@
 class SellersController < ApplicationController
   def index
     @sellers = Seller.all
+
+    @num_products_for_sellers = hash_of_num_products_for_each_seller(@sellers)
   end
 
   def show
     @seller = Seller.find(params[:id])
+  end
+
+  private
+
+  # these two methods below help calculate a seller's
+  # total number of products in stock
+  def calculate_num_products(seller)
+    num_products = 0
+    seller.products.each do |product|
+      num_products += product.stock
+    end
+    return num_products
+  end
+
+  def hash_of_num_products_for_each_seller(sellers)
+    hash = {}
+    sellers.each do |seller|
+      hash[seller.id] = calculate_num_products(seller)
+    end
+    return hash
   end
 end
