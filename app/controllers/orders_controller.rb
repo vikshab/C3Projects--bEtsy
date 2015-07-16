10703b976006f1b80311d757163a8978a9630a93
Brittany L. Walentin <brittany.walentin@gmail.com>
2015-07-15 19:58:52 -0700

    CRUD for orders controller complete.  No tests made yet.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,43 @@
 class OrdersController < ApplicationController
+
+  def new
+    @order = Order.new
+  end
+
+
+  def create
+    @order = Order.create(order_params)
+
+    if @order.save
+      redirect_to root_path #need to change this when we have other views
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @order = Order.find(id: order_params[:id])
+    @order.destroy
+
+  end
+
+  def index
+    @all_orders = Order.all
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:buyer_name,
+                                  :buyer_email,
+                                  :buyer_zip,
+                                  :buyer_state,
+                                  :buyer_address,
+                                  :buyer_city,
+                                  :buyer_expcc,
+                                  :buyer_last4cc)
+  end
+
+
+
 end
