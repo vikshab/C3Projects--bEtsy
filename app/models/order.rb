0e8102fe2bebcf5a4de9c95f3a80b2cdd9a85b94
Brittany L. Walentin <brittany.walentin@gmail.com>
2015-07-16 13:37:28 -0700

    added more specific validations. No custom validations.

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,12 +5,23 @@ class Order < ActiveRecord::Base
   validates :buyer_name, presence: true
   validates :buyer_email, presence: true
   validates :buyer_address, presence: true
-  validates :buyer_zip, presence: true
-  validates :buyer_state, presence: true
+  validates :buyer_zip, presence: true,
+                        numericality: { only_integer: true },
+                        length: { minimum: 5 }
+  validates :buyer_state, presence: true,
+                          length: { is: 2 }
   validates :buyer_city, presence: true
   validates :buyer_expcc, presence: true
-  validates :buyer_last4cc, presence: true
+  validates :buyer_last4cc, presence: true,
+                            numericality: { only_integer: true },
+                            length: { is: 4 }
+
+  # custom VALIDATIONS
+  # validate :
 
   # SCOPES ------------------------------------------------------------
+
+
+
 
 end
