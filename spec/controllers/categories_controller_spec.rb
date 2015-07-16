7d8412c00b710638520a1265c2932a5f0af21ea1
Ashley Watkins <AWatkins31@gmail.com>
2015-07-16 09:43:36 -0700

    Adds Category#show view + tests

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -16,6 +16,27 @@ RSpec.describe CategoriesController, type: :controller do
     Category.create( { name: "Electronic" } )
   }
 
+  let(:create_4_products) {
+    Product.create( { name: "books", price: "600", seller_id: "7", stock: "2" } )
+    Product.create( { name: "sunglasses", price: "10000", seller_id: "1", stock: "9" } )
+    Product.create( { name: "coffee", price: "1000", seller_id: "6", stock: "4" } )
+    Product.create( { name: "t-shirts", price: "1300", seller_id: "9", stock: "15" } )
+  }
+
+  describe "private methods" do
+    describe "@set_category" do
+      it "finds category and sets it to @category" do
+        params = {}
+        allow(controller).to receive(:params).and_return(id: category.id)
+        expect(controller.send(:set_category)).to eq(category)
+      end
+
+      context "when category does not exist" do
+        # TODO: What do we want to have happen if someone goes to a URL that doesn't exist?
+      end
+    end
+  end
+
   describe "Actions" do
     describe "GET #index" do
       it "renders the index template" do
@@ -29,6 +50,31 @@ RSpec.describe CategoriesController, type: :controller do
         get :index
         expect(assigns(:categories)).to eq(Category.all)
         expect(assigns(:categories).count).to eq(11)
+      end
+    end
+
+    describe "GET #show" do
+      it "renders the show template" do
+        get :show, { id: category.id }
+        expect(response).to render_template("show")
+      end
+
+      it "assigns @category (through before_action)" do
+        get :show, { id: category.id }
+        expect(assigns(:category)).to eq(category)
+      end
+
+      it "assigns @products for the given @category" do # TODO: the wording here seems odd
+        create_4_products
+        category.products << Product.all
+
+        get :show, { id: category.id }
+        expect(assigns(:products)).to eq(Product.all)
+        expect(assigns(:products).count).to eq(4)
+      end
+
+      context "when a cateogry has no products" do
+        # TODO: what do we want to have happen when there are no products?
       end
     end
   end
