d1cbb66ff5243179caede59afe6bf6f70f8d94a8
Ashley Watkins <AWatkins31@gmail.com>
2015-07-16 09:41:34 -0700

    Adds Categories#index view + tests

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe CategoriesController, type: :controller do
+  let(:category) { Category.create( { name: "I am a category" } ) }
+  let(:create_11_categories) {
+    Category.create( { name: "Grocery" } )
+    Category.create( { name: "Animals" } )
+    Category.create( { name: "Home and Garden" } )
+    Category.create( { name: "Toys" } )
+    Category.create( { name: "Clothing" } )
+    Category.create( { name: "Health and Beauty" } )
+    Category.create( { name: "Sports and Outdoors" } )
+    Category.create( { name: "Automotive" } )
+    Category.create( { name: "Books" } )
+    Category.create( { name: "Movies" } )
+    Category.create( { name: "Electronic" } )
+  }
 
+  describe "Actions" do
+    describe "GET #index" do
+      it "renders the index template" do
+        get :index
+        expect(response).to render_template("index")
+      end
+
+      it "assigns @categories" do
+        create_11_categories
+
+        get :index
+        expect(assigns(:categories)).to eq(Category.all)
+        expect(assigns(:categories).count).to eq(11)
+      end
+    end
+  end
 end
