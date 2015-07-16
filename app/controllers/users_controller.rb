790e8071126a2733e49719cf544bf839926d6e85
Elia <elgrenier@gmail.com>
2015-07-15 17:36:56 -0700

    starting testing users controller, working on model validations and views to support TDD

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,26 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def create_params
+    params.permit(user: [:name, :email, :password_digest])
+  end
+
+
 end
