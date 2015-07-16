d525a7c3b274cdbaec76be4d89ac0519fbcd9e4c
Lindsey Reno <lindseyannereno@gmail.com>
2015-07-16 14:58:30 -0700

    Adds validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,8 @@
 class Review < ActiveRecord::Base
   belongs_to :products
+
+  # Validations ----------------------------------------------------------------
+  validates :rating, presence: true, numericality: { only_integer: true,
+            greater_than_or_equal_to: 1, less_than_or_equal_to: 5  }
+  validates :product_id, presence: true, numericality: { only_integer: true }
 end
