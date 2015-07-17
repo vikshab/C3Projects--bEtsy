62e3fa248f1e48aa223b26abbf5d01c4a82cbd1a
Elia <elgrenier@gmail.com>
2015-07-16 18:44:24 -0700

    able to fix the many to many table issue with products and categories

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150716233228) do
+ActiveRecord::Schema.define(version: 20150717012326) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "categories_products", id: false, force: :cascade do |t|
+    t.integer "category_id"
+    t.integer "product_id"
+  end
+
+  add_index "categories_products", ["category_id"], name: "index_categories_products_on_category_id"
+  add_index "categories_products", ["product_id"], name: "index_categories_products_on_product_id"
 
   create_table "order_items", force: :cascade do |t|
     t.integer  "quantity"
@@ -57,14 +65,6 @@ ActiveRecord::Schema.define(version: 20150716233228) do
   end
 
   add_index "products", ["user_id"], name: "index_products_on_user_id"
-
-  create_table "products_categories", id: false, force: :cascade do |t|
-    t.integer "product_id"
-    t.integer "category_id"
-  end
-
-  add_index "products_categories", ["category_id"], name: "index_products_categories_on_category_id"
-  add_index "products_categories", ["product_id"], name: "index_products_categories_on_product_id"
 
   create_table "reviews", force: :cascade do |t|
     t.text     "body"
--- db/seeds.rb
+++ db/seeds.rb
@@ -19,10 +19,12 @@ CSV.foreach("db/products.csv", headers: true) do |row|
   )
  end
 
-category_products = { 1 => 1, 1 => 2, 1 => 4, 1 => 5, 1 => 9, 1 => 12, 2 => 1, 2 => 3, 2 => 12, 3 => 7, 3 => 13, 3 => 14, 3 => 15, 3 => 16, 3 => 17, 3 => 18, 4 => 6, 4 => 7, 4 => 8, 4 => 10, 4 => 11, 4 => 15, 4 => 18, 4 => 14 }
+category_products = { 1 => [1, 2, 4, 5, 9, 12], 2 => [1, 3, 12 ], 3 => [7, 13, 14, 15, 16, 17, 18], 4 => [6, 7, 8, 10, 11, 15, 18, 14] }
 # fix this to be a hash of key/value pairs
 
 category_products.each do |k, v|
-category = Category.find(k)
-category.products << Product.find(v)
+  category = Category.find(k)
+  v.each do |p|
+    category.products << Product.find(p)
+  end
 end
