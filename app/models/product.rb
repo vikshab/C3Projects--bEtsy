65e2f37e7f650d0219c2cb206dcb9ab433bedec0
Anita <asovay@gmail.com>
2015-07-16 18:50:05 -0700

    fix product price validation to be float instead of integer

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ActiveRecord::Base
 # Validations___________________________________________________________________
  validates :name, presence: true, uniqueness: true
- validates :price, presence: true, numericality: {only_integer: true, greater_than: 0}
+ validates :price, presence: true, numericality: {only_float: true, greater_than: 0}
 
 # Assosiations__________________________________________________________________
   belongs_to :category
