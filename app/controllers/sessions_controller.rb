bae5e1253caaee2a60865c027baa02763fa57be4
Brittany L. Walentin <brittany.walentin@gmail.com>
2015-07-16 17:12:22 -0700

    Login/out logic done. WIP: logout path.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,17 +9,17 @@ class SessionsController < ApplicationController
     # login using email address
       if @user && @user.authenticate(params[:session][:password]) # user was found
         session[:user_id] = @user.id # setting session to the user.id
-        binding.pry
-        redirect_to root_path
+        redirect_to user_path(@user.id)
       else # user wasn't found
-        flash.now[:error] = "Try Again Matey" #@user.errors.messages
+        flash.now[:error] = "Please try again Bitsy Baby"
         render "new"
       end
   end
 
   def destroy
+    # session[:user_id] = @user.id
     session[:user_id] = nil
-    redirect_to root_path
+    render root_path
   end
 
 end
