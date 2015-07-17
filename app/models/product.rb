c1ccfa57dbbb555cefd4d7e44d15ccc98a96348f
Anita <asovay@gmail.com>
2015-07-16 23:15:18 -0700

    removed welcome banner from layout and added to only the homepage

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,9 @@
 class Product < ActiveRecord::Base
-# Validations___________________________________________________________________
+# Validations ------------------------------------------------------------------
  validates :name, presence: true, uniqueness: true
  validates :price, presence: true, numericality: {only_float: true, greater_than: 0}
 
-# Assosiations__________________________________________________________________
+# Assosiations -----------------------------------------------------------------
   has_and_belongs_to_many :categories
   belongs_to :user
   has_many :reviews
