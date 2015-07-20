4b840916fc6dcc10c87f1f7fae0b4bc00d01c308
Corinne Pingul <corinnepingul@gmail.com>
2015-07-20 15:36:37 -0700

    Deleted comments from WelcomeController.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,4 @@
 class WelcomeController < ApplicationController
   def index
-    # # TODO: Jeri, decide if we should keep or delete this
-    # session[:order_id] ||= Order.create(status: "pending").id
   end
 end
