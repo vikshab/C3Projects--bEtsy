3c768cf1b874cd72f625455ef1cfacbd58619398
Anita <asovay@gmail.com>
2015-07-19 23:29:15 -0700

    added yellow stars

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def render_stars(rating)
+    output = ''
+    if (1..5).include?(rating.to_i)
+      rating.to_i.times { output += image_tag('star.png') }
+    end
+    output.html_safe
+  end
+
 end
