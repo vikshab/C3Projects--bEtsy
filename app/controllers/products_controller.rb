1b189885b1a07b2b6efd71adbb6c417561515a8b
Lindsey Reno <lindseyannereno@gmail.com>
2015-07-20 21:52:16 -0700

    Adds edit and update actions to products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  before_action :set_product, only: [:show, :add_to_cart]
+  before_action :set_product, only: [:show, :edit]
 
   def index
     @products = Product.all
@@ -10,9 +10,23 @@ class ProductsController < ApplicationController
     @average_rating = @product.average_rating
   end
 
+  def edit; end
+
+  def update
+    if @product.update(create_params)
+      redirect_to product_path(@product)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def set_product
     @product = Product.find(params[:id])
+  end
+
+  def create_params
+    params.require(:product).permit(:name, :price, :seller_id, :stock, :description, :photo_url)
   end
 end
