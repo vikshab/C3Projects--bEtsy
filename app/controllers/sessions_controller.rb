5adec47b7d4fa1776f5b543069bd6dcb112dc6a1
Victoria <vikshab2010@hotmail.com>
2015-07-21 15:55:31 -0700

    Added sesseion[order_id]=nil to destroy method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -19,6 +19,7 @@ class SessionsController < ApplicationController
   def destroy
     # session[:user_id] = @user.id
     session[:user_id] = nil
+    session[:order_id] = nil
     redirect_to root_path
   end
 
