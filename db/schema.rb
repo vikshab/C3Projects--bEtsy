6d24c3749ebaedbb2b8f5814ba08f7c4ab1ed597
Anita <asovay@gmail.com>
2015-07-20 19:05:24 -0700

    added buyer model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150720001601) do
+ActiveRecord::Schema.define(version: 20150720232631) do
+
+  create_table "buyers", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "address"
+    t.string   "city"
+    t.string   "state"
+    t.integer  "zip"
+    t.integer  "last4cc"
+    t.string   "expirationcc"
+    t.integer  "order_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  add_index "buyers", ["order_id"], name: "index_buyers_on_order_id"
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
