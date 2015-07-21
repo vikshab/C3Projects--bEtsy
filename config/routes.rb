c76c28c2df492fa05db8eb6301dca7cb92dacc0a
Lindsey Reno <lindseyannereno@gmail.com>
2015-07-20 22:53:49 -0700

    Adds routes to create a product

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,12 @@ Rails.application.routes.draw do
 
   resources :categories, except: :destroy
   resources :sellers, only: [:index, :show]
-  resources :products, except: :destroy
+  resources :products, except: [:destroy, :new, :create]
 
   get '/products/:id/reviews/new', to: 'reviews#new', as: "new_review"
   post '/products/:id/reviews/new', to: 'reviews#create'
+  get '/sellers/:id/products/new', to: 'products#new', as: "new_product"
+  post '/sellers/:id/products/new', to: 'products#create'
 
   scope :cart do
     get "/", to: "orders#cart", as: "cart"
