2acdc25186ac88eba0a960266f2c264f73419623
Victoria <vikshab2010@hotmail.com>
2015-07-21 16:52:46 -0700

    Added new validation to quantity

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,7 @@ class Product < ActiveRecord::Base
  validates :name, presence: true, uniqueness: true
  validates :price, presence: true, numericality: { greater_than: 0 }
  validates :user_id, presence: true
+ validates :stock, numericality: {greater_than_or_equal_to: 0}
 
 # ASSOCIATIONS -----------------------------------------------------------------
 
