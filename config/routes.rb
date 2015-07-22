717a3a3d86f88f86c58fc1c4e501930b62a0e0cd
Lindsey Reno <lindseyannereno@gmail.com>
2015-07-22 11:00:14 -0700

    Adds a dashboard for a seller

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy", as: "logout"
 
+  get '/sellers/dashboard', to: 'sellers#dashboard', as: "dashboard"
+
   resources :categories, except: :destroy
   resources :sellers, only: [:index, :show, :new, :create] do
     resources :products, only: [:new, :create]
