c0f6c6bd065607554895b0d26d1a75cf8b348135
Anita <asovay@gmail.com>
2015-07-21 22:37:01 -0700

    added checkout path and default email value if user is logged in

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -1,12 +1,7 @@
 class BuyersController < ApplicationController
 
   def new
-    if session[:user_id]
-      @user = User.find(session[:user_id])
-    else
-      @user = ""
-    end
-
+    @user = User.find(session[:user_id])
   end
   
 end
