a9ea242015604931a19637b8c3ef56f815d0a966
Anita <asovay@gmail.com>
2015-07-21 21:10:46 -0700

    added logic to allow logged in merchant to only review other peoples products and not their own

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,7 +27,7 @@ class ApplicationController < ActionController::Base
   def require_login
     unless session[:user_id]
       flash[:error] = "You must be logged in to access this section"
-   redirect_to login_path
+      redirect_to login_path
     end
   end
 end
