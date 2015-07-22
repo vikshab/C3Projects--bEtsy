c3ecb9f1e46002f824f96e71519111cac472b1ad
Victoria <vikshab2010@hotmail.com>
2015-07-22 14:35:39 -0700

    Status changed to 'paid'

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -28,10 +28,9 @@ class BuyersController < ApplicationController
       inventory = item.product.stock #inventory
       inventory = inventory - bought
       product.update(stock: inventory)
-
-      item.order.update(status: "paid")
-
     end
+
+    @order.update(status: "paid")
     session[:order_id] = nil
   end
 
