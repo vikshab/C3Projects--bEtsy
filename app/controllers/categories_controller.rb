a9d067b1f9decfd905340fa375456af0db8495ff
Lindsey Reno <lindseyannereno@gmail.com>
2015-07-23 14:39:07 -0700

    Adds ability for a seller to create a category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,8 @@
 class CategoriesController < ApplicationController
   before_action :set_category, only: [:show]
+  before_action :set_seller, only: [:new, :create]
+  before_action :require_seller_login, only: [:new, :create]
+
   def index
     @categories = Category.all.sort_by { |category| category.name }
   end
@@ -8,8 +11,28 @@ class CategoriesController < ApplicationController
     @products = @category.products
   end
 
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(create_params)
+    if @category.save
+      flash[:messages] = MESSAGES[:successful_create_category]
+      session[:seller_id] = @seller.id
+      redirect_to seller_products_path(@seller)
+    else
+      flash.now[:errors] = @category.errors
+      render :new
+    end
+  end
+
   private
     def set_category
       @category = Category.find(params[:id])
+    end
+
+    def create_params
+      params.require(:category).permit(:name)
     end
 end
