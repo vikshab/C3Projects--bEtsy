f8c7fee9b55a702a781d9caae7816c3434230649
Victoria <vikshab2010@hotmail.com>
2015-07-23 15:49:02 -0700

    All green tests for user_controller 100% coverage

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,28 +2,17 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
 
-  describe "a user can see their user page" do
+  # SHOW and NEW ACTIONS_________________________________________________________________
+
+  describe "GET #show and GET #new" do
     before :each do
-      @user = User.create(name: "Name1", email: "name@email.com", password_digest: "foobar")
+      @user = User.create(name: "first_user", email: "name@email.com", password_digest: "foobar")
       session[:user_id] = @user.id
-    end
-
-    it "responds successfully with an HTTP 200 status code" do
-      get :show, id: @user.id
-
-      expect(response).to be_success
-      expect(response).to have_http_status(200)
     end
 
     it "renders the #show view" do
       get :show, id: @user.id
       expect(response).to render_template("show")
-    end
-
-    it "responds successfully with an HTTP 200 status code" do
-      get :new, user_id: @user
-      expect(response).to be_success
-      expect(response).to have_http_status(200)
     end
 
     it "renders the #new view" do
@@ -32,50 +21,62 @@ RSpec.describe UsersController, type: :controller do
     end
   end
 
-  #
-  #   it "increases the rank when you upvote" do
-  #     patch :upvote, id: @album
-  #     @album.reload
-  #     expect(@album.rank).to eq(1)
-  #   end
-  #
-  #   it "deletes a given album" do
-  #     delete :destroy, id: @album
-  #     expect(Album.count).to eq(0)
-  #   end
-  #
-  # end
-  #
-  # describe "makes new albums" do
-  #   let(:valid_album) do {
-  #     album: { name: "name1"}
-  #   }
-  #   end
-  #
-  #   it "creates a new Album" do
-  #     post :create, valid_album
-  #     expect(Album.count).to eq(1)
-  #   end
-  #
-  #   it "redirects to the album show page" do
-  #     post :create, valid_album
-  #     expect(response).to redirect_to(album_path(assigns(:media)))
-  #   end
-  # end
-  #
-  # describe "albums can be edited" do
-  #   let(:album) {Album.create(name: "name1", rank: 20)}
-  #
-  #   it "updates an album with valid params" do
-  #     post :update, id: album, album: {name: "Edited name", rank: 20}
-  #     album.reload
-  #     expect(album.name).to eq("Edited name")
-  #   end
-  #
-  #   it "redirects to the album show page" do
-  #     post :update, id: album, album: {name: "Edited name", rank: 20}
-  #     expect(response).to redirect_to(album_path(assigns(:media)))
-  #   end
+  # CREATE ACTION_________________________________________________________________
 
+  describe "POST #create" do
 
+    context "Valid user params" do
+      before :each do
+        @user = User.new(user_params[:user])
+      end
+
+      let(:user_params) do
+        {
+          user: {
+            name: 'second_user',
+            email: 'first_user@email.com',
+            password: 'ComplicatedPassword',
+            password_confirmation: 'ComplicatedPassword'
+          }
+        }
+      end
+
+      it "creates a new user" do
+        post :create, user_params
+        expect(User.count).to eq 1 # I've created a new user above, so there should be 2
+      end
+
+      it "redirects to the user show page" do
+        post :create, user_params
+        expect(subject).to redirect_to(user_path(assigns(:user)))
+      end
+    end
+
+    context "Invalid user params" do
+      before :each do
+        @user = User.new(user_params[:user])
+      end
+
+      let(:user_params) do
+        {
+          user: {
+            name: '',
+            email: 'third_user@email.com',
+            password: 'ComplicatedPassword',
+            password_confirmation: 'ComplicatedPassword'
+          }
+        }
+      end
+
+      it "does not persist invalid user" do
+        post :create, user_params
+        expect(User.count).to eq 0
+      end
+
+      it "renders the :new view if the user didn't enter a name" do
+        post :create, user_params
+        expect(response).to render_template("new")
+      end
+    end
+  end
 end
