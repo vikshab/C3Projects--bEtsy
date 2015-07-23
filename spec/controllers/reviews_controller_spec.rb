33d74d093dc17c3d4cf440457f6f501eaac76e23
Anita <asovay@gmail.com>
2015-07-23 16:06:28 -0700

    reviews controller tests

--- spec/controllers/reviews_controller_spec.rb
+++ spec/controllers/reviews_controller_spec.rb
@@ -2,4 +2,30 @@ require 'rails_helper'
 
 RSpec.describe ReviewsController, type: :controller do
 
+  describe "POST #create" do
+    let(:review_params) do { review: { rating: 4 } } end
+    let(:invalid_params) do { review: { body: "text" } } end
+    before :each do
+      @product = Product.create(name: "name", price: 10.99, user_id: 2, stock: 1) 
+    end 
+    
+    context "valid params" do 
+      it "creates a Media record" do
+        post :create, { product_id: @product.id }.merge(review_params)
+        expect(Review.count).to eq 1
+      end
+
+      it "redirects to the product show page" do
+        post :create, { product_id: @product.id }.merge(review_params)
+        expect(subject).to redirect_to(product_path(@product.id))
+      end
+    end
+
+    context "invalid params" do
+      it "does not persist invalid records" do
+        post :create, { product_id: @product.id }.merge(invalid_params)
+        expect(Review.count).to eq 0
+      end
+    end
+  end
 end
