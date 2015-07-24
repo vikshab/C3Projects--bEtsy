a3cd6173a890129eb35c3f8472d205c5dc3e638b
Jeri <drvonnjerryxlii@gmail.com>
2015-07-24 09:36:26 -0700

    added products helper specs for #quantity_sold & #retire_text_short

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -3,12 +3,13 @@ module ProductsHelper
     "#{ product.retired ? "Reactivate" : "Retire" } this Product"
   end
 
-  def retire_text_short(product) # TODO: spec
+  def retire_text_short(product)
     "#{ product.retired ? "Reactivate" : "Retire" }"
   end
 
-  def quantity_sold(product) # TODO: spec
-    quantities = product.order_items.map { |item| item.quantity_ordered }
+  def quantity_sold(product)
+    quantities = product.order_items.select { |item| item.order.status == "paid" }
+    quantities = quantities.map { |item| item.quantity_ordered }
     total = quantities.reduce(0) { |sum, current_quantity| sum += current_quantity }
   end
 end
--- spec/helpers/products_helper_spec.rb
+++ spec/helpers/products_helper_spec.rb
@@ -18,4 +18,37 @@ RSpec.describe ProductsHelper, type: :helper do
       expect(retire_product_text(product)).to eq("Retire this Product")
     end
   end
+
+  describe "#retire_text_short" do
+    it "outputs some text for the retire product button" do
+      product = Product.create(name: "asdfasjkhdf", stock: 1, price: 1, seller_id: 1)
+
+      expect(retire_text_short(product)).to eq("Retire")
+    end
+
+    it "alters the text based on the product's retired status" do
+      product = Product.create(name: "asdfasjkhdf", stock: 1, price: 1, seller_id: 1)
+
+      product.retire!
+      expect(retire_text_short(product)).to eq("Reactivate")
+
+      product.retire!
+      expect(retire_text_short(product)).to eq("Retire")
+    end
+  end
+
+  describe "#quantity_sold" do
+    it "calculates the quantity of of a product actually sold based on order_item qty & order status" do # OPTIMIZE rephrase this?
+      checkout_params = { buyer_name: "asdflkasd asdflk asfd", buyer_email: "asdf@eml.net",
+        buyer_address: "1234 fake st", buyer_card_expiration: Date.parse("June 5 2086"),
+        buyer_card_short: "4567", status: "paid" }
+      product = Product.create(name: "asdfasjkhdf", stock: 100, price: 1, seller_id: 1)
+      order1 = Order.create
+      OrderItem.create(quantity_ordered: 10, product_id: product.id, order_id: order1.id)
+      order2 = Order.create(checkout_params)
+      OrderItem.create(quantity_ordered: 10, product_id: product.id, order_id: order2.id)
+
+      expect(quantity_sold(product)).to eq(10)
+    end
+  end
 end
