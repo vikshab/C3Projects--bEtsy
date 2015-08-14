d2ae583d83b1269a924b9e156cca4cc214188e9c
Victoria <vikshab2010@hotmail.com>
2015-08-14 13:16:08 -0700

    Moved repeatable part to before action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
   before_action :correct_user, except: [:new, :create]
+  before_action :find_user, only: [:show, :correct_user]
   # need this so it can see the helper methods
   include ApplicationHelper
 
   def show
-    @user = User.find(params[:id])
     @products = @user.products
   end
 
@@ -23,7 +23,6 @@ class UsersController < ApplicationController
   end
 
   def correct_user
-    @user = User.find(params[:id])
     redirect_to(root_url) unless current_user?(@user)
   end
 
@@ -31,5 +30,9 @@ class UsersController < ApplicationController
 
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    def find_user
+      @user = User.find(params[:id])
     end
 end
