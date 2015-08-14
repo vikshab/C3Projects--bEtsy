4e84a43b87c186f222fd50ec4383a9064153a8d2
Brittany L. Walentin <brittany.walentin@gmail.com>
2015-08-14 12:47:16 -0700

    product_params separated onto multiple lines for increased visibility. In #create method elsif changed to else.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -56,7 +56,10 @@ class ProductsController < ApplicationController
     end
 
     def product_params
-      params.require(:product).permit(:name, :price, :desc, :stock, :photo_url, :user_id, :retired, :category_ids => [], :categories_attributes => [:id, :name])
+      params.require(:product).permit(
+        :name, :price, :desc, :stock, :photo_url,
+        :user_id, :retired, :category_ids => [],
+        :categories_attributes => [:id, :name])
     end
 
     def merchant_exist?
