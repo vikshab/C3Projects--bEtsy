8c4ff2233c7c8f4fd8003371de9ffd044c954f7a
Victoria <vikshab2010@hotmail.com>
2015-08-14 13:47:25 -0700

    Deleted the whole method validate email, added just format: /@/ to the model validation, test is green

--- app/models/buyer.rb
+++ app/models/buyer.rb
@@ -6,7 +6,7 @@ class Buyer < ActiveRecord::Base
 
   # VALIDATIONS ----------------------------------------------------------
   validates :name,        presence: true
-  validates :email,       presence: true
+  validates :email,       presence: true, format: /@/
   validates :address,     presence: true
   validates :zip,         presence: true,
                           numericality: { only_integer: true },
@@ -19,9 +19,6 @@ class Buyer < ActiveRecord::Base
                           numericality: { only_integer: true },
                           length: { in: 14..16 }
 
-  validate :email_must_contain_at
-
-
   private
 
     def convert_to_last_four
@@ -29,13 +26,5 @@ class Buyer < ActiveRecord::Base
       return self.credit_card unless array.length >= 4
       last_four = array[-4..-1].join.to_i
       self.credit_card = last_four
-    end
-
-    def email_must_contain_at
-      return if self.email == nil # guard clause, inline conditional
-      unless self.email.chars.include?("@")
-        # refactor to include regex
-        errors.add(:email, "Invalid email. Please enter a correct email address.")
-      end
     end
 end
