315e148f6db92b77f8fb060530d96d4fe7c6d235
Elia <elgrenier@gmail.com>
2015-08-16 20:18:55 -0700

    moving view logic into a helper

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -4,4 +4,8 @@ module OrdersHelper
     items = OrderItem.where("order_id = ?", order.id).pluck(:quantity)
     @total_quantity = items.sum
   end
+
+  def out_of_stock
+    @order_items.any? { |order_item| order_item.quantity > order_item.product.stock } |
+  end
 end
